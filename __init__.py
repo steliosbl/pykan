1599d1f353710b6002e3576c9ce93a637f05c9e8
Stelios Boulitsakis Logothetis <stelios@logothet.is>
2025-12-15 12:27:21 +0000

    Fixed relative imports

--- __init__.py
+++ __init__.py
@@ -1,34 +1,15 @@
-# Inject submodule paths into sys.modules to allow imports like "from kan.LBFGS import X"
-# This must be done before any other imports to avoid circular dependencies
+from .kan.MultKAN import MultKAN, KAN
+from .kan.utils import *
+
+# Expose nested kan modules as top-level kan.* modules
+from .kan import utils, LBFGS, KANLayer, Symbolic_KANLayer, spline, MLP, compiler
 import sys
+sys.modules['kan.utils'] = utils
+sys.modules['kan.LBFGS'] = LBFGS
+sys.modules['kan.KANLayer'] = KANLayer
+sys.modules['kan.Symbolic_KANLayer'] = Symbolic_KANLayer
+sys.modules['kan.spline'] = spline
+sys.modules['kan.MLP'] = MLP
+sys.modules['kan.compiler'] = compiler
 
-# Import the submodules directly and register them
-import kan.kan.LBFGS
-import kan.kan.utils
-sys.modules['kan.LBFGS'] = sys.modules['kan.kan.LBFGS']
-sys.modules['kan.utils'] = sys.modules['kan.kan.utils']
-
-# Now import everything from the inner kan.kan package
-from kan.kan import *
-
-# Explicitly expose key classes
-from kan.kan.MultKAN import KAN, MultKAN
-from kan.kan.KANLayer import KANLayer
-from kan.kan.Symbolic_KANLayer import Symbolic_KANLayer
-from kan.kan.MLP import MLP
-from kan.kan.LBFGS import LBFGS
-
-# Make utils available
-from kan.kan import utils
-
-__all__ = [
-    'KAN',
-    'MultKAN', 
-    'LBFGS',
-    'KANLayer',
-    'Symbolic_KANLayer',
-    'MLP',
-    'utils',
-]
-
-#torch.use_deterministic_algorithms(True)
+__all__ = ['MultKAN', 'KAN', 'utils', 'LBFGS', 'KANLayer', 'Symbolic_KANLayer', 'spline', 'MLP', 'compiler']
